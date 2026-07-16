3ae2aab3300d7682d9f3b73b123aebd5d609d5a8
falloumolina-glitch <falloumolina@gmail.com>
2026-07-16 00:21:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+const data = [
+    {
+        titre: "Créer un CV professionnel",
+        description: "Guide pour créer un bon CV.",
+        mot: "cv"
+    },
+    {
+        titre: "Formations gratuites",
+        description: "Découvrir des ressources pour apprendre.",
+        mot: "formation"
+    },
+    {
+        titre: "Programmation web",
+        description: "Apprendre HTML, CSS et JavaScript.",
+        mot: "code"
+    }
+];
+
 function search() {
+    let query = document.getElementById("search").value.toLowerCase();
+    let result = document.getElementById("result");
 
-    let query = document.getElementById("search").value;
+    let found = data.filter(item => 
+        item.mot.includes(query)
+    );
 
-    document.getElementById("result").innerHTML =
-    "Recherche de : " + query;
-
-}
+    if(found.length > 0) {
+        result.innerHTML = found.map(item =>
+            `<h3>${item.titre}</h3>
+             <p>${item.description}</p>`
+        ).join("");
+    } else {
+        result.innerHTML = "Aucun résultat trouvé";
+    }
+                                     }
