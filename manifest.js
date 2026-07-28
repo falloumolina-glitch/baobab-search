3035124c2c2b360fb22b27ed467c1a57cbb2f6b9
falloumolina-glitch <falloumolina@gmail.com>
2026-07-28 15:11:33 +0000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,11 +2,20 @@
   "name": "Baobab Search",
   "short_name": "Baobab",
   "start_url": "./index.html",
+  "scope": "./",
   "display": "standalone",
   "background_color": "#ffffff",
   "theme_color": "#1A73E8",
   "icons": [
-    {"src": "icon-192.png", "sizes": "192x192", "type": "image/png"},
-    {"src": "icon-512.png", "sizes": "512x512", "type": "image/png"}
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
   ]
 }
